033d464dbbe55f5812d63022072c8a7b94904b97
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2018-01-14 14:44:39 -0600

    backend works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :bucket_lists
   has_many :list_items, through: :bucket_lists
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'root#index'
   resources :bucket_lists, only: [:destroy]
   resources :list_items, except: [:destroy]
+
   resources :users, only: [:show, :create, :update] do
+
+    collection do
+      post '/login', to: 'users#login'
+    end
     resources :bucket_lists, only: [:create]
   end
 end
