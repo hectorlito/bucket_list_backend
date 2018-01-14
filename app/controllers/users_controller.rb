70dd5f136fa06ae5f4cf07552159dd8ee529890e
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2018-01-14 11:58:26 -0600

    auth is broken, 500 error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   # before_action :set_user, only: [:show, :update]
+  before_action :authenticate_token, except: %i[login create]
 
   # GET /users
-  # def index
-  #   @users = User.all
-  #
-  #   render json: @users.to_json(include: :list_items)
-  # end
+  def index
+    @users = User.all
+
+    render json: @users
+    # .to_json(include: :list_items)
+  end
 
   # GET /users/1
   def show
@@ -34,14 +36,40 @@ class UsersController < ApplicationController
   #   end
   # end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  def login
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      token = create_token(user.id, user.username)
+      render json: { status: 200, token: token, user: user }
+    else
+      render json: { status: 401, message: 'Unauthorized' }
     end
+end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:username, :password_digest, :avatar)
-    end
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def create_token(id, username)
+    JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+  end
+
+  def payload(id, username)
+  {
+    exp: (Time.now + 30.minutes).to_i,
+    iat: Time.now.to_i,
+    iss: ENV['JWT_ISSUER'],
+    user: {
+      id: id,
+      username: username
+    }
+  }
+end
+def user_params
+  params.require(:user).permit(:username, :password_digest, :avatar)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root 'root#index'
-  resources :bucket_lists, only: [:destroy]
-  resources :list_items, except: [:destroy]
-  resources :users, only: [:show, :create] do
-    resources :bucket_lists, only: [:create]
+  # root 'root#index'
+  # resources :bucket_lists, only: [:destroy]
+  # resources :list_items, except: [:destroy]
+  resources :users do
+    # , only: [:show, :create]
+    collection do
+      post '/login', to: 'users#login'
+    end
+    # resources :bucket_lists, only: [:create]
+
   end
 end
