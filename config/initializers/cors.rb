9c4bc028b1b7bb56f3d6ae300d031184639ae8e3
Paige Boyer <paige1381@gmail.com>
2018-01-15 14:39:25 -0500

    changed port to 8101

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
   allow do
-    origins 'localhost:8100'
+    origins 'localhost:8101'
 
     resource '*',
       headers: :any,
