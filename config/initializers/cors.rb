1fb0c7bdac2d602ab306de7faf022bf15b9fe858
Paige Boyer <paige1381@gmail.com>
2018-01-15 09:32:10 -0500

    added origin for mobile app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+
+  allow do
+    origins 'localhost:8100/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
 end
