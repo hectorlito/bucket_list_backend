55a47450b9182b311d96481b6d26f6b9a0c660e6
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2018-01-15 22:10:27 -0600

    unknoen commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      # login
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
