a97c6165cd9c649ae7ba0262c6aaf5bfe1d5180e
Paige Boyer <paige1381@gmail.com>
2018-01-17 15:35:05 -0500

    todo and completed routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'root#index'
-  resources :bucket_lists, only: [:create, :destroy, :show]
+  resources :bucket_lists, only: [:create, :destroy, :update, :show]
   resources :list_items, except: [:destroy]
 
   resources :users, only: [:show, :create, :update] do
